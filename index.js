a937e6353769d3d17019713a2451d4ee2a5e88ce
Armans0701~ <sikder89098@gmail.com>
2023-05-20 16:26:56 +0600

    added git ignore

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors =require ('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,9 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json());
 
+console.log(process.env.DB_PASS)
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.xxlectq.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxlectq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,6 +30,15 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+app.post("/addtoy",(req,res)=>{
+    const body = req.body;
+    console.log(body);
+})
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
